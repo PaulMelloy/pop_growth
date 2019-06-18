2f3f3b580ae2ff4051f121ebbfba7f809cc8e60f
melloy <paul@melloy.com.au>
2019-06-18 17:03:19 +1000

    save changes

--- Gapminder_population_gganimate.R
+++ Gapminder_population_gganimate.R
@@ -8,10 +8,14 @@ head(gapminder)
 
 
 p.1 <- gapminder %>%
-ggplot(aes(y = pop, x = lifeExp, colour = country))+
-   geom_point(alpha = 0.6)+
+ggplot(aes(y = pop, x = lifeExp,size = gdpPercap, colour = country))+
+   geom_point(show.legend = FALSE,alpha = 0.6)+
    scale_color_viridis_d() +
+   scale_size(range = c(2, 12)) +
+   scale_y_log10() +
    labs(x = "Life Expectancy", y = "Population")
 
-p.1 + transition_time(year)+
-   labs(title = "Year: {frame_time}")
+p.1 + facet_wrap(~continent)+
+   transition_time(year)+
+   labs(title = "Year: {frame_time}")+
+   shadow_wake(wake_length = 0.1, alpha = FALSE)
